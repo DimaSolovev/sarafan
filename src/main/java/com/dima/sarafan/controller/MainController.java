33725b1bf267c0304c12ad47a67917004ef021a4
Dima <soloveid87@mail.ru>
2022-05-31 14:56:19 +1000

    session, logout, thymeleaf

---
+++ src/main/java/com/dima/sarafan/controller/MainController.java
@@ -0,0 +1,35 @@
+package com.dima.sarafan.controller;
+
+import com.dima.sarafan.domain.User;
+import com.dima.sarafan.repo.MessageRepo;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    private final MessageRepo messageRepo;
+
+    public MainController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
+    @GetMapping
+    public String main(Model model, @AuthenticationPrincipal User user) {
+
+        HashMap<Object, Object> data = new HashMap<>();
+
+        data.put("profile", user);
+        data.put("messages", messageRepo.findAll());
+
+        model.addAttribute("frontendData", data);
+
+        return "index";
+    }
+}
