afe3cb722716bc26ac4bff2dde35dac72cab1142
Dima <soloveid87@mail.ru>
2022-06-09 15:19:27 +1000

    graph

--- src/main/java/com/dima/sarafan/domain/Message.java
+++ src/main/java/com/dima/sarafan/domain/Message.java
@@ -28,5 +28,12 @@ public class Message {
     @JsonView(Views.FullMessage.class)
     private LocalDateTime creationDate;
 
-
+    @JsonView(Views.FullMessage.class)
+    private String link;
+    @JsonView(Views.FullMessage.class)
+    private String linkTitle;
+    @JsonView(Views.FullMessage.class)
+    private String linkDescription;
+    @JsonView(Views.FullMessage.class)
+    private String linkCover;
 }
