cda26db91fb2b2decc363c0509341defd238ba6e
Dima <soloveid87@mail.ru>
2022-06-10 11:48:04 +1000

    comments front

--- src/main/resources/static/js/components/messages/MessageRow.vue
+++ src/main/resources/static/js/components/messages/MessageRow.vue
@@ -4,23 +4,27 @@
             <i>({{ message.id }})</i>
             {{ message.text }}
         </v-card-text>
+        <media v-if="message.link" :message="message"></media>
         <v-card-actions>
-            <v-btn rounded @click="edit" small flat>Edit</v-btn>
-            <v-btn rounded @click="del" small>Delete</v-btn>
-            <v-icon
-                    large
-                    color="green darken-2"
-            >
-                mdi-domain
-            </v-icon>
+            <v-btn value="Edit" @click="edit" small flat round>Edit</v-btn>
+            <v-btn icon @click="del" small>
+                <v-icon>delete</v-icon>
+            </v-btn>
         </v-card-actions>
+        <comment-list
+                :comments="message.comments"
+                :message-id="message.id"
+        ></comment-list>
     </v-card>
 </template>
 
 <script>
     import { mapActions } from 'vuex'
+    import Media from 'components/media/Media.vue'
+    import CommentList from '../comment/CommentList.vue'
     export default {
         props: ['message', 'editMessage'],
+        components: { CommentList, Media },
         methods: {
             ...mapActions(['removeMessageAction']),
             edit() {
