f1eb31de90f5e1d84cd128e924e7b26d3946ce7a
Dima <soloveid87@mail.ru>
2022-06-21 11:35:45 +1000

    sentry

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,8 +7,27 @@ import App from 'pages/App.vue'
 import store from 'store/store'
 import { connect } from './util/ws'
 import 'vuetify/dist/vuetify.min.css'
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
 
-if (frontendData.profile) {
+Sentry.init({
+    dsn: "https://81bbb365eb5f4687ad725e445670bef0@o1294585.ingest.sentry.io/6518851",
+    integrations: [
+        new Integrations.Vue({
+            Vue,
+            attachProps: true,
+        }),
+    ],
+})
+
+Sentry.configureScope(scope =>
+    scope.setUser({
+        id: profile && profile.id,
+        username: profile && profile.name
+    })
+)
+
+if (profile) {
     connect()
 }
 
